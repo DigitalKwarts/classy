076343a8adb617daaf2a6841d9f083d81569cc40
Andrew FMH <andrew@flosites.org>
2016-05-09 14:47:23 +0300

    Implemented initial theme config structure and loader

---
+++ custom/config.php
@@ -0,0 +1,3 @@
+<?php 
+
+$textdomain = 'themename';
---
+++ framework/init.php
@@ -0,0 +1,139 @@
+<?php 
+
+/**
+ * The core theme class.
+ *
+ *
+ * @since      1.0.0
+ * @package    ThemeFramework
+ * @author     Andrew Tolochka <atolochka@gmail.com>
+ */
+class ThemeFramework {
+
+	/**
+	 * Singleton instance of plugin
+	 *
+	 * @var ThemeFramework
+	 * @since  0.1.0
+	 */
+	protected static $single_instance = null;
+
+	/**
+	 * Creates or returns an instance of this class.
+	 *
+	 * @since  0.1.0
+	 * @return ThemeFramework A single instance of this class.
+	 */
+	public static function get_instance() {
+
+		if ( null === self::$single_instance ) {
+			self::$single_instance = new self();
+		}
+
+		return self::$single_instance;
+
+	}
+
+	/**
+	 * Define the core functionality of the them.
+	 *
+	 * Set the theme name and the theme version that can be used throughout the theme.
+	 *
+	 * @since    1.0.0
+	 */
+	protected function __construct() {
+
+		$this->define_constants();
+
+		$this->init_config();
+
+	}
+
+	/**
+	 * Function to define constants
+	 * 
+	 * @param  string
+	 * @param  string
+	 */
+	private function define( $name, $value ) {
+		
+		if ( !defined($name) ) {
+			
+			define( $name, $value );
+
+		}
+
+	}
+
+	/**
+	 * Defines plugin constants
+	 * 
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_constants() {
+
+		$theme = wp_get_theme();
+
+		$this->define( 'THEME', $theme->template );
+		$this->define( 'THEME_NAME', $theme->get('Name') );
+		$this->define( 'THEME_PATH', get_template_directory() . '/' );
+		$this->define( 'THEME_DIR', get_template_directory_uri() . '/' );
+		$this->define( 'THEME_VERSION', $theme->get('Version') );
+		$this->define( 'THEME_FRAMEWORK_PATH', THEME_PATH . 'framework/' );
+		$this->define( 'THEME_FRAMEWORK_DIR', THEME_DIR . 'framework/' );
+
+	}
+
+	/**
+	 * Init Theme Configuration
+	 */
+	private function init_config() {
+
+		// Require Class
+		require_once THEME_FRAMEWORK_PATH . 'core/theme-config.php';
+
+		$this->config = ThemeConfig::init();
+
+	}
+
+	public static function get_config_var($name) {
+
+		$vars = ThemeConfig::get_vars();
+
+		if (isset($vars[$name])) return $vars[$name];
+
+		return false;
+
+	}
+
+	public static function textdomain() {
+
+		$textdomain = ThemeFramework::get_config_var('textdomain');
+
+		return $textdomain ? $textdomain : THEME;
+
+	}
+
+}
+
+
+/**
+ * Grab the ThemeFramework object and return it.
+ * Wrapper for ThemeFramework::get_instance()
+ *
+ * @since  0.1.0
+ * @return ThemeFramework  Singleton instance of plugin class.
+ */
+function get_theme_framework() {
+	
+	return ThemeFramework::get_instance();
+
+}
+
+/**
+ * Get Instance
+ * 
+ * @var ThemeFramework
+ */
+$themeframework = get_theme_framework(); 
