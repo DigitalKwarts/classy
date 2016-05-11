f8a95e0757d69937542aa8cfc0b99ea3c524517a
Andrew FMH <atolochka@gmail.com>
2016-05-11 07:51:05 +0300

    Started implementation of user class

---
+++ app/models/classy-user.php
@@ -0,0 +1,19 @@
+<?php 
+
+class ClassyUser {
+
+	/**
+	 * Current user id
+	 * @var int
+	 */
+	public $id;
+
+	
+	/**
+	 * Main constructor function. Requires user id
+	 * @param int $uid
+	 */
+	public function __construct($uid = null) {
+	}
+
+}
