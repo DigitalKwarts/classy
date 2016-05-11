ab82748af378a260902b15daea31f39eabf18657
Andrew FMH <atolochka@gmail.com>
2016-05-11 14:15:12 +0300

    Implemented default index view

--- views/index/index.blade.php
+++ views/index/index.blade.php
@@ -1,5 +1,36 @@
 @extends('basic')
 
 @section('content')
-	<h1>{{ $foo }}</h1>
+	<h1>{{ $page_title }}</h1>
+
+	@forelse ($posts as $post)
+		<article>
+			<h3><a href="{{ $post->permalink() }}">{{ $post->title() }}</a></h3>
+		</article>
+	@empty
+		<p>No posts</p>
+	@endforelse
+
+	<div class="pagination">
+		@if ($pagination->prev)
+			<a href="{{ $pagination->prev->link }}" class="prev {{ $pagination->prev->link }}">Prev</a>
+		@endif
+
+		<ul class="pages">
+			@foreach ($pagination->pages as $page)
+				<li>
+					@if (isset($page->link))
+						<a href="{{ $page->link }}" class="{{ $page->class }}">{{ $page->title }}</a>
+					@else
+						<span class="{{ $page->class }}">{{ $page->title }}</span>
+					@endif
+				</li>
+			@endforeach
+		</ul>
+
+		@if ($pagination->next)
+			<a href="{{ $pagination->next->link }}" class="next {{ $pagination->next->link}}">Next</a>
+		@endif
+	</div>
+	
 @stop
