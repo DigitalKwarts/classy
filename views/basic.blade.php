e755fdf5803d4d902323fdc052e1651e3eab4d62
Andrew FMH <atolochka@gmail.com>
2016-05-11 07:52:15 +0300

    Added pingback, rss, favicon

--- views/basic.blade.php
+++ views/basic.blade.php
@@ -4,6 +4,9 @@
         <meta charset="{{ bloginfo( 'charset' ) }}" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="pingback" href="{{ bloginfo('pingback_url') }}" />
+        <link rel="alternate" type="application/rss+xml" title="{{ bloginfo('name') }} RSS Feed" href="{{ bloginfo('rss2_url') }}" />
+        <link rel="shortcut icon" href="{{ THEME_DIR }}assets/favicon.ico" />
         <title>{{ wp_title('|', true, 'right'); }}</title>
         {{ wp_head() }}
     </head>
