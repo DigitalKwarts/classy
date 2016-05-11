f580f536c09446debafd99807210c23de7e5bc2e
Andrew FMH <atolochka@gmail.com>
2016-05-11 11:20:44 +0300

    Implemented basic user model

--- app/models/classy-user.php
+++ app/models/classy-user.php
@@ -6,7 +6,49 @@ class ClassyUser {
 	 * Current user id
 	 * @var int
 	 */
-	public $id;
+	public $ID;
+
+	/**
+	 * User posts url
+	 * @var string
+	 */
+	public $link;
+
+	/**
+	 * User login (example: anrw)
+	 * @var string
+	 */
+	public $user_login;
+
+	/**
+	 * It's basically sanitized version of user login, used for permalinks
+	 * @var string
+	 */
+	public $user_nicename;
+
+	/**
+	 * User email
+	 * @var string
+	 */
+	public $user_email;
+
+	/**
+	 * Human-Friendly name, like: Andrew Tolochka
+	 * @var string
+	 */
+	public $display_name;
+
+	/**
+	 * User password hash
+	 * @var string
+	 */
+	private $user_pass;
+
+	/**
+	 * Stores current post object
+	 * @var object
+	 */
+	private $object;
 
 	
 	/**
@@ -14,6 +56,101 @@ class ClassyUser {
 	 * @param int $uid
 	 */
 	public function __construct($uid = null) {
+		$this->ID = $this->verify_id($uid);
+		
+		$this->init();
+	}
+
+	private function verify_id($uid) {
+		return $uid;
+	}
+
+	/**
+	 * Initialises Instance based on provided post id
+	 */
+	private function init() {
+		$object = (array) $this->get_object();
+
+		foreach ($object['data'] as $key => $value) {
+			$this->$key = $value;
+		}
+	}
+
+	/**
+	 * Returns user object
+	 * 
+	 * @return object
+	 */
+	private function get_object() {
+		return get_userdata($this->ID);
+	}
+
+	/**
+	 * Returns user first name
+	 * 
+	 * @return string
+	 */
+	public function first_name() {
+
+		return $this->first_name;
+	
+	}
+
+	/**
+	 * Returns user display name
+	 * 
+	 * @return string
+	 */
+	public function display_name() {
+
+		return $this->display_name;
+
+	}
+	
+	/**
+	 * Returns user full name
+	 * 
+	 * @return string
+	 */
+	public function full_name() {
+
+		return $this->first_name . ' ' . $this->last_name;
+
+	}
+	
+	/**
+	 * Returns user user_login
+	 * 
+	 * @return string
+	 */
+	public function user_login() {
+
+		return $this->user_login;
+
+	}
+	
+	/**
+	 * Returns user email
+	 * 
+	 * @return string
+	 */
+	public function email() {
+
+		return $this->user_email;
+
+	}
+
+	/**
+	 * Returns author posts url
+	 * 
+	 * @return string
+	 */
+	public function link() {
+		if ( !$this->link ) {
+			$this->link = get_author_posts_url($this->ID);
+		}
+
+		return $this->link;
 	}
 
 }
