992329b2ee9b28d981a258e28bf68b0d914e3658
Andrew FMH <atolochka@gmail.com>
2016-05-11 07:49:45 +0300

    Started implementation of image class

---
+++ app/models/classy-image.php
@@ -0,0 +1,19 @@
+<?php 
+
+class ClassyImage {
+
+	/**
+	 * Current image id
+	 * @var int
+	 */
+	public $id;
+
+	
+	/**
+	 * Main constructor function. Requires image id
+	 * @param int $pid
+	 */
+	public function __construct($pid = null) {
+	}
+
+}
