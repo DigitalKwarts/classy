cd1fe9644024473fe8e7f23e2e88e6a488436fb1
Andrew FMH <atolochka@gmail.com>
2016-05-13 07:26:59 +0300

    Basic view for page

---
+++ views/page/page.blade.php
@@ -0,0 +1,13 @@
+@extends('base.default')
+
+@section('content')
+	@if ($post)
+		<article>
+			<h1>{{ $post->title() }}</h1>
+			<section class="body">
+				{{ $post->content() }}
+			</section>
+			{{ comments_template() }}
+		</article>
+	@endif
+@stop
