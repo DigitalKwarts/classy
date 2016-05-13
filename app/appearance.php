ba270e02ad2fcca320d46ef531538570d98d20a9
Andrew FMH <atolochka@gmail.com>
2016-05-13 07:31:32 +0300

    Added setup_theme method, added thumbnails, html5 support and nav menu register

--- app/appearance.php
+++ app/appearance.php
@@ -15,6 +15,8 @@ class ClassyAppearance {
 
 		add_action( 'wp_print_scripts', array($this, 'init_js_vars') );
 
+		add_action( 'after_setup_theme', array($this, 'setup_theme') );
+
 	}
 
 	/**
@@ -75,6 +77,41 @@ class ClassyAppearance {
 			'theme',
 			$options
 		);
+
+	}
+
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	public function setup_theme() {
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+		 */
+		
+		add_theme_support('post-thumbnails');
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus(array(
+			'header-menu' => __('Header Menu', Classy::textdomain()),
+			'footer-menu' => __('Footer Menu', Classy::textdomain()),
+		));
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		
+		add_theme_support('html5', array(
+			'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+		));
 
 	}
 
