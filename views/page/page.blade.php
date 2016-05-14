0354fe23292a1f49950976e7dc6ca26f8058f0ab
Andrew FMH <atolochka@gmail.com>
2016-05-14 17:06:21 +0300

    Small tweaks

--- views/page/page.blade.php
+++ views/page/page.blade.php
@@ -4,10 +4,10 @@
 	@if ($post)
 		<article>
 			<h1>{{ $post->title() }}</h1>
+			
 			<section class="body">
 				{{ $post->content() }}
 			</section>
-			{{ comments_template() }}
 		</article>
 	@endif
 @stop
--- views/single/single.blade.php
+++ views/single/single.blade.php
@@ -4,9 +4,11 @@
 	@if ($post)
 		<article>
 			<h1>{{ $post->title() }}</h1>
+			
 			<section class="body">
 				{{ $post->content() }}
 			</section>
+
 			{{ comments_template() }}
 		</article>
 	@endif
