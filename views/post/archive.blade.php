63ca1e105d0f75e5fe7f9d3d3d2a58b1991bac02
Andrew FMH <atolochka@gmail.com>
2016-06-22 16:44:26 +0300

    Moved post preview, pagination to separate files

--- views/post/archive.blade.php
+++ views/post/archive.blade.php
@@ -3,36 +3,14 @@
 @section('content')
 	<h1>{{ $page_title }}</h1>
 
-	@forelse ($posts as $post)
-		<article>
-			<h3><a href="{{ $post->permalink() }}">{{ $post->title() }}</a></h3>
-		</article>
-	@empty
-		<p>No posts</p>
-	@endforelse
-
-	@if ($pagination)
-		<div class="pagination">
-			@if (isset($pagination->prev))
-				<a href="{{ $pagination->prev->link }}" class="prev {{ $pagination->prev->link }}">Prev</a>
-			@endif
-
-			<ul class="pages">
-				@foreach ($pagination->pages as $page)
-					<li>
-						@if (isset($page->link))
-							<a href="{{ $page->link }}" class="{{ $page->class }}">{{ $page->title }}</a>
-						@else
-							<span class="{{ $page->class }}">{{ $page->title }}</span>
-						@endif
-					</li>
-				@endforeach
-			</ul>
-
-			@if (isset($pagination->next))
-				<a href="{{ $pagination->next->link }}" class="next {{ $pagination->next->link}}">Next</a>
-			@endif
-		</div>
+	@if (isset($posts))
+		@forelse ($posts as $post)
+			@include ('post.preview')
+		@empty
+			<p>No posts</p>
+		@endforelse
 	@endif
+
+	@include ('layout.pagination')
 	
 @stop
