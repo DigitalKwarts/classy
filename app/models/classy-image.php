6b035871e0072d06f052d3997206a125347d7dd6
Andrew FMH <atolochka@gmail.com>
2016-06-22 15:44:19 +0300

    Implemented basic image functionality

--- app/models/classy-image.php
+++ app/models/classy-image.php
@@ -6,13 +6,54 @@ class ClassyImage extends ClassyBasis {
 	 * Current image id
 	 * @var int
 	 */
-	public $id;
-
+	public $ID;
 
 	/**
 	 * Main constructor function. Requires image id
 	 * @param int $pid
 	 */
 	public function __construct( $pid = null ) {
+
+		// Checks if image with this id exists
+		if ( $pid && wp_get_attachment_image_src( $pid ) ) {
+
+			$this->ID = $pid;
+
+		} else {
+
+			$this->ID = 0;
+
+		}
+	}
+
+	/**
+	 * Returns default image url
+	 * @return string
+	 */
+	public static function get_default_image() {
+
+		// You can put here any url
+		return THEME_DIR . '/assets/noimage.png';
+
+	}
+
+	/**
+	 * Returns image url. In case image ID is 0 or not set returns default image
+	 * @param  string $size
+	 * @return string
+	 */
+	public function src( $size = 'medium' ) {
+
+		if ( $this->ID ) {
+
+			$thumb = wp_get_attachment_image_src( $this->ID, $size );
+
+			return $thumb[0];
+
+		} else {
+
+			return self::get_default_image();
+
+		}
 	}
 }
