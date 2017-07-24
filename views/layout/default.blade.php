1351d0a6d16d3696416102b8d0a779f0acb4792a
Andrew FMH <atolochka@gmail.com>
2017-07-24 11:19:34 +0300

    Updated windwalker/renderer & illuminate/view

--- views/layout/default.blade.php
+++ views/layout/default.blade.php
@@ -7,7 +7,7 @@
         <link rel="pingback" href="{{ bloginfo('pingback_url') }}" />
         <link rel="alternate" type="application/rss+xml" title="{{ bloginfo('name') }} RSS Feed" href="{{ bloginfo('rss2_url') }}" />
         <link rel="shortcut icon" href="{{ CLASSY_THEME_DIR }}assets/favicon.ico" />
-        <title>{{ wp_title('|', true, 'right'); }}</title>
+        <title>{{ wp_title('|', true, 'right') }}</title>
         {{ wp_head() }}
     </head>
     <body {{ body_class() }}>
